c4a85f83d721e696b139875f80bb8da6fa88be3f
gapi <>
2023-06-03 18:52:43 +0200

    Fixed directions

--- core/util.go
+++ core/util.go
@@ -5,14 +5,18 @@ type point struct {
     x, y uint
 }
 
+// (1,1) -> center
+// (0,0) (1,0) (2,0)
+// (0,1) (1,1) (2,1)
+// (0,2) (1,2) (2,2)
 func dir2point(dir Direction, p point) point {
     switch dir {
     case NORD:
-        return point{p.x, p.y+1}
+        return point{p.x, p.y-1}
     case WEST:
         return point{p.x-1, p.y}
     case SOUTH:
-        return point{p.x, p.y-1}
+        return point{p.x, p.y+1}
     case EAST:
         return point{p.x+1, p.y}
     }
