9c4876f152ff4de03f73cba665c541edf5be8390
gapi <>
2023-06-07 15:28:22 +0200

    Added comments to grid code

--- core/grid.go
+++ core/grid.go
@@ -6,14 +6,16 @@ type Grid struct {
 
     width, height uint
 
-    // Used for smart block loading, could be changed in future
+    // Used for smart cells loading, could be changed in future
     updatePoints []point
 }
 
+// Get size of the grid
 func (grid Grid) Dimensions() (uint, uint) {
     return grid.width, grid.height
 }
 
+// Initialization of the grid
 func (grid *Grid) Init(w, h uint) {
     grid.width  = w
     grid.height = h
@@ -24,12 +26,16 @@ func (grid *Grid) Init(w, h uint) {
     }
 }
 
+// Returns pointer to cell at x, y
 func (grid *Grid) GetCell(x, y uint) *Cell {
     index := y*grid.width + x
     return &grid.cells[index]
 }
 
+// Updates the grid
 func (grid *Grid) Update() {
+    // Gets list of points with forced update
+    // XXX: Could be moved to SetCell and Init methods for a better perfomance?
     for x := uint(0); x < grid.width; x++ {
         for y := uint(0); y < grid.height; y++ {
             if (*grid.GetCell(x, y)).forcedUpdate() {
@@ -37,6 +43,9 @@ func (grid *Grid) Update() {
             }
         }
     }
+
+    // New list of update points for that points that are not forced
+    // BUG: It doesn't work??? (Needs some tests)
     newUpdate := []point{}
     for _, p := range(grid.updatePoints) {
         cell := grid.GetCell(p.x, p.y)
@@ -44,9 +53,13 @@ func (grid *Grid) Update() {
         // Passing grid 3x3 around cell to Update method
         // TODO: 5x5?
         var g [3][3](*Cell)
+        // Local x and y in grid 3x3
         var i, j uint = 0, 0
+        // Loop over area 3x3 around cell
         for x := p.x-1; x <= p.x+1; x++ {
             for y := p.y-1; y <= p.y+1; y++ {
+                // Because x & y are uint, they can't be below 0, so just check
+                // is it greater than width || height
                 if x > grid.width || y > grid.height { continue }
                 g[i][j] = grid.GetCell(x, y)
                 i++
@@ -54,6 +67,8 @@ func (grid *Grid) Update() {
             i = 0
             j++
         }
+
+        // Cell returns a list of points that needs to be loaded in next tick
         points := (*cell).Update(g)
         for _, rp := range(points) {
             newUpdate = append(newUpdate, point{rp.x+p.x-1, rp.y+p.y-1})
